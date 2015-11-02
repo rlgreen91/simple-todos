85d14609a09e0d23c987b3ff29d2c07227fea54f
Rachel Green <rlgreen91@gmail.com>
2015-11-01 22:58:25 -0600

    Initial commit

---
+++ simple-todos.js
@@ -0,0 +1,11 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+}
+
