b198b2796cb2c54c5bf89307629224a3b2b41168
Rachel Green <rlgreen91@gmail.com>
2015-11-02 17:32:24 -0600

    Can now hide comleted tasks and will display the number of incomplete tasks

--- simple-todos.js
+++ simple-todos.js
@@ -3,10 +3,21 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function () {
-      //Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
+  	tasks: function() {
+  		if (Session.get("hideCompleted")) {
+  			//If hide completed is checked, filter tasks
+  			return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+  		} else {
+  			//Otherwise, return all of the tasks
+  			return Tasks.find({}, {sort: {createdAt: -1}});
+  		}
+  	},
+  	hideCompleted: function() {
+  		return Session.get("hideCompleted");
+  	},
+  	incompleteCount: function() {
+  		return Tasks.find({checked: {$ne: true}}).count();
+  	}
   });
 
   Template.body.events({
@@ -25,6 +36,10 @@ if (Meteor.isClient) {
 
   		//Clear form
   		event.target.text.value = "";
+  	},
+
+  	"change .hide-completed input": function(event) {
+  		Session.set("hideCompleted", event.target.checked);
   	}
   });
 
